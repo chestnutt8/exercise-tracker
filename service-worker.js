ed8e104fdf5b2763a67d5639abbba09a1f490a89
chestnutt8 <jxhej230@gmail.com>
2026-06-05 22:00:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install',e=>{
-e.waitUntil(caches.open('v1').then(c=>c.addAll(['./','index.html','styles.css','app.js','manifest.json'])));
+e.waitUntil(caches.open('v2').then(c=>c.addAll(['./','index.html','styles.css','app.js','manifest.json'])));
 });
 self.addEventListener('fetch',e=>{
 e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
